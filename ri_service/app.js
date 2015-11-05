8d0da4bede6d73e4c957007e943dda11d8fe5f0a
wakahiu <wakahiu@berkeley.edu>
2015-11-04 22:25:43 -0800

    Merge fixing ref int

--- ri_service/app.js
+++ ri_service/app.js
@@ -1,14 +1,7 @@
 var redis = require("redis")
 
-<<<<<<< HEAD
 clientRISub = redis.createClient() // Subscribes to ri channel
 clientRIPub = redis.createClient()// Publishes to student channel
-=======
-clientRI = redis.createClient() // Subscribes to ri channel
-clientStudent = redis.createClient()// Publishes to student channel
-clientCourse = redis.createClient()
->>>>>>> 92fc740f531341847799091bfa8f623278f90ddc
-
 clientRISub.subscribe("referential_integrity");
 
 clientRISub.on("subscribe", function (channel, count) {
@@ -24,43 +17,14 @@ clientRISub.on("message", function (channel, message) { // Listens for ri channe
 
     // Publish to the students microservice
     if (obj.sender == "courses_micro_service") {
-<<<<<<< HEAD
         publish_channel = "students_micro_service";      
-=======
-        // Parse the JSON message and publish message to student channel
-        switch (obj.action) {
-            case "update student add course":
-            break;
-            case "update student delete course":
-            break;
-            case "delete course":
-            break;
-        }
-        
-        clientStudent.publish("students microservice", message);
-        console.log("Published to students microservice \t" +  message)
-        
->>>>>>> 92fc740f531341847799091bfa8f623278f90ddc
+
     }
     
     // Publish to the courses microservice 
     else if (obj.sender == "students_micro_service") {
-<<<<<<< HEAD
         publish_channel = "courses_micro_service";
-=======
-        // Parse the JSON message and publish message to course channel
-        switch (obj.action) {
-            case "update course add student":
-            break;
-            case "update course delete student":
-            break;
-            case "delete student"
-            break;
-        }
 
-        clientCourse.publish("courses microservice", message);
-        console.log("Published to courses microservice" +  message)
->>>>>>> 92fc740f531341847799091bfa8f623278f90ddc
     }
 
     clientRIPub.publish(publish_channel, message);
