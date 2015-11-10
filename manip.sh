b9e4603addb0f43e1da7325e543523323ac79ff7
wakahiu <wakahiu@berkeley.edu>
2015-11-09 20:10:36 -0500

    Snapshot log

--- manip.sh
+++ manip.sh
@@ -1,3 +1,11 @@
+for i in {1000..1020}
+
+do
+	echo "Welcome $i times"
+	curl -X POST http://localhost:3001/courses?course_num=$i
+done
+
+
 curl -i -X PUT -H 'Content-Type: application/json' -d '{"course":"1122"}' http://localhost:3003/jsc2226/remove-course
 
 curl -i -X DELETE -H 'Content-Type: application/json' -d '{"students":{"lastname":"Singh","firstname":"Jivtesh","uni":"jsc2226"}}' http://localhost:3000/courses/1122
--- students_service/config/env/development.js
+++ students_service/config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	port: process.env.PORT || 3001,
+	port: process.env.PORT || 3002,
 	db: 'mongodb://0.0.0.0/students_service_1',
 	starting_uni: '<starting_char>',
 	ending_uni: '<ending_char>'
